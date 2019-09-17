d7622737e6091767e3447480e162f484937ca5fc
tktezy <tkey5074@gmail.com>
2019-09-17 21:55:05 +0300

    Created home page (controller Home), added toute home#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 devise_scope :user do
-    root to: 'devise/registrations#new'
+    root to: 'home#index'
   end
 end
