9e2713158faaa286728556eb17483efa87a5d1b5
tktezy <tkey5074@gmail.com>
2019-09-17 22:38:06 +0300

    Added temporarily display of all rigestred users. Added links to sign-in, sign-out to a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 devise_scope :user do
     root to: 'home#index'
-  end
+    get "sign_up" => "devise/sessions#new"
+    get "sign_out" => "devise/sessions#destroy"
+end
+
 end
