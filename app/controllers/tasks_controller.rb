7cd94797648d4ab49243016ae0af7d151e90ac16
AlexAndrianov <Aliaksei_Andryianau@epam.com>
2019-09-23 12:50:13 +0300

    fixes for edit/update view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,10 +9,12 @@ class TasksController < ApplicationController
 	end
 
 	def edit
+		@task = Task.find(params[:id])
 	end
 
 
 	def update
+		@task = Task.find(params[:id])
 		if @task.update(task_params)
 			redirect_to project_path(@project)
 		else
