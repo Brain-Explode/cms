0a9082f6e4b43d46cfefd8c50b8f104086222aa4
AlexAndrianov <alexandrianovmail@gmail.com>
2019-10-15 09:50:05 +0300

    devise login still not working

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,12 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "Projects", type: :request do
+  include Devise::Test::ControllerHelpers
   describe "request list of all projects" do
     it "responds with list of all projects" do
+      binding.pry
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      admin_user = FactoryBot.create(:admin_user)
+      sign_in :admin_user, admin_user # sign_in(scope, resource
       project = build(:random_project)
       # task = Task.new(title: 'qwe', body: "body", project_id: project.id)
       get projects_path
-      binding.pry
       expect(response).to be_successful
       expect(response.body).to include(“Test user”)
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
 require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -35,6 +35,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   require 'support/factory_bot'
+  config.include Devise::Test::ControllerHelpers, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
