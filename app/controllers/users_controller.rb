0e62d911f845593adee82b91ce495a446b6034cc
tktezy <tkey5074@gmail.com>
2019-10-22 21:59:22 +0300

    Projects now has&belongs to many users. Trying to fix bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,17 +12,17 @@ class UsersController < ApplicationController
   def show; end
 
   def update
-    if @user.update(user_params)
-      redirect_to root_path, notice: 'User was successfully updated.'
+    if @users.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
     else
       #I like it
-      redirect_to root_path, notice: 'Wake up Neo, you obosralsya.'
+      redirect_to users_path, notice: 'Wake up Neo, you obosralsya.'
     end
   end
 
   private
     def set_user
-      @user = User.find(params[:id])
+      @users = User.find(params[:id])
     end
 
     def user_params
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-	belongs_to :user
+	has_and_belongs_to_many :users
 	has_many   :tasks, dependent: :destroy
 	validates  :project_title, presence: true
 	has_one_attached :file
