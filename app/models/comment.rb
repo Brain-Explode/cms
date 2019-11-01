3144337359c8ab06ab54eabf6b9688754da1cffa
AlexAndrianov <alexandrianovmail@gmail.com>
2019-11-01 09:58:12 +0300

    activity feed 1st version

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,15 @@
 class Comment < ApplicationRecord
-	belongs_to :task
+  include PublicActivity::Model
+  tracked owner: ->(controller, model) { controller && controller.current_user }
+
+  attr_accessor :remove_images, :boolean
+  attr_accessor :remove_files, :boolean
+
+  belongs_to :task
 	belongs_to :user
 	has_many_attached :files
 	has_many_attached :images
 	resourcify
-
-  attr_accessor :remove_images, :boolean
-  attr_accessor :remove_files, :boolean
 
   after_save :purge_images
   after_save :purge_files
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+  include PublicActivity::Model
+  tracked owner: ->(controller, model) { controller && controller.current_user }
+
 	belongs_to :user
 	belongs_to :project
 	has_many   :comments, dependent: :destroy
