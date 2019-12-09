2b5fe5b22b845bbed1c8caaa3ae3addc400895bf
tktezy <tkey5074@gmail.com>
2019-12-09 19:49:10 +0300

    added folder 'workers' and rb file

---
+++ app/workers/report_worker.rb
@@ -0,0 +1,9 @@
+class ReportWorker
+	include Sidekiq::Worker
+	sidekiq_options retry: false
+
+	def perform(start_date, end_date)
+		puts "SIDEKIQ worker generating a report from #{start_date} to #{end_date}"
+	end
+
+end
